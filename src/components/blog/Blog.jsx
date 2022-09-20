08eb67ea66f0b644cb757305c32e938b075b359a
Shuqroh <92368644+Shuqroh@users.noreply.github.com>
2022-09-20 10:09:01 +0100

    Add files via upload

---
+++ src/components/blog/Blog.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Badge, Button, Card, Col, Form, Stack } from "react-bootstrap";
+import { truncateAddress } from "../../utils/conversions";
+import Identicon from "../utils/Identicon";
+import EditBlog from "./EditBlog";
+import ViewBlog from "./ViewPost";
+
+const Blog = ({
+  address,
+  blog,
+  editBlog,
+  deleteBlog,
+  upvoteBlog,
+  downvoteBlog,
+}) => {
+  const { title, image, appId, owner } = blog;
+
+  return (
+    <Col key={appId}>
+      <Card className="h-100">
+        <Card.Header>
+          <Stack direction="horizontal" gap={2}>
+            <span className="font-monospace text-secondary">
+              {truncateAddress(owner)}
+            </span>
+            <Identicon size={28} address={owner} />
+            <Badge bg="secondary" className="ms-auto">
+              Published
+            </Badge>
+          </Stack>
+        </Card.Header>
+        <div className="ratio ratio-4x3">
+          <img src={image} alt={title} style={{ objectFit: "cover" }} />
+        </div>
+        <Card.Body className="d-flex flex-column text-center">
+          <Card.Title>{title}</Card.Title>
+          {/* <Card.Text className="flex-grow-1">{location}</Card.Text> */}
+          <Form className="d-flex align-content-stretch flex-row gap-2">
+            {blog.owner !== address ? (
+              <EditBlog blog={blog} editBlog={editBlog} />
+            ) : (
+              <ViewBlog blog={blog} />
+            )}
+            <Button
+              variant="outline-dark"
+              onClick={() => upvoteBlog(blog)}
+              className="btn"
+            >
+              {blog.upvote}
+              <i className="bi bi-hand-thumbs-up-fill"></i>
+            </Button>
+            <Button
+              variant="outline-dark"
+              onClick={() => downvoteBlog(blog)}
+              className="btn"
+            >
+              {blog.downvote}
+              <i className="bi bi-hand-thumbs-down-fill"></i>
+            </Button>
+            {blog.owner === address && (
+              <Button
+                variant="outline-danger"
+                onClick={() => deleteBlog(blog)}
+                className="btn"
+              >
+                <i className="bi bi-trash"></i>
+              </Button>
+            )}
+          </Form>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
+
+Blog.propTypes = {
+  address: PropTypes.string.isRequired,
+  blog: PropTypes.instanceOf(Object).isRequired,
+  editBlog: PropTypes.func.isRequired,
+  deleteBlog: PropTypes.func.isRequired,
+  upvoteBlog: PropTypes.func.isRequired,
+  downvoteBlog: PropTypes.func.isRequired,
+};
+
+export default Blog;
---
+++ src/components/blog/ViewPost.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Badge, Button, Modal, Stack } from "react-bootstrap";
+import Identicon from "../utils/Identicon";
+import { truncateAddress } from "../../utils/conversions";
+
+const ViewBlog = ({ blog }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button
+        variant="outline-dark"
+        onClick={handleShow}
+        className="btn w-50 py-3"
+      >
+        Read More
+      </Button>
+
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>{blog.title}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <div className="ratio ratio-4x3">
+            <img
+              src={blog.image}
+              alt={blog.title}
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+          <p>{blog.content}</p>
+          <h6>Author</h6>
+          <Stack direction="horizontal" gap={2}>
+            <span className="font-monospace text-secondary">
+              {truncateAddress(blog.owner)}
+            </span>
+            <Identicon size={28} address={blog.owner} />
+            <Badge bg="secondary" className="ms-auto">
+              Published
+            </Badge>
+          </Stack>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="outline-secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+ViewBlog.propTypes = {
+  blog: PropTypes.instanceOf(Object).isRequired,
+};
+
+export default ViewBlog;
